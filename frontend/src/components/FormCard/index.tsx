cfca6d3b051fdc68b7ae5277c29d0d0984094a27
dagsonpatrick <dagsonmg@yahoo.com.br>
2022-01-24 21:55:40 -0300

    Removendo imports desnecessarios

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { Socket } from 'dgram';
+
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
